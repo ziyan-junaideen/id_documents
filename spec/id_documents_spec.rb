dc3c7044004ff3da848e7259a6d4895d9f08e78c
Ziyan Junaideen <ziyan@jdeen.com>
2023-11-12 02:04:01 +0530

    Remove failing spec

--- spec/id_documents_spec.rb
+++ spec/id_documents_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe IDDocuments do
   it "has a version number" do
     expect(IDDocuments::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
