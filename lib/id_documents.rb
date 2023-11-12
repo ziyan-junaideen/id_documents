87d755cf35de1dad51649910cd9afeed33da6bcf
Ziyan Junaideen <ziyan@jdeen.com>
2023-11-12 10:07:31 +0530

    Introduce Zeitwerk (#2)
    * zeitwerk with basic config
    
    * Add Linux
    
    * Remove failing spec
    
    * Rubocop corrections

--- lib/id_documents.rb
+++ lib/id_documents.rb
@@ -1,1 +1,11 @@
+# frozen_string_literal: true
 
+require "zeitwerk"
+require_relative "id_documents/inflector"
+
+loader = Zeitwerk::Loader.for_gem
+loader.inflector = IDDocuments::Inflector.new(__FILE__)
+loader.collapse("#{__dir__}/id_documents/rules")
+loader.inflector.inflect("id_documents" => "IDDocuments")
+loader.inflector.inflect("id_card" => "IDCard")
+loader.setup
---
+++ lib/id_documents/inflector.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module IDDocuments
+  class Inflector < Zeitwerk::GemInflector
+    def camelize(basename, abspath)
+      if basename =~ /\A[a-z]{3}\z/ && abspath =~ %r{/rules/[a-z]{3}}
+        basename.upcase
+      else
+        super
+      end
+    end
+  end
+end
