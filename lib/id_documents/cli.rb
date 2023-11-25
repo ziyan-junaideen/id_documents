5a048f542bd25487fac15408e5de5413b592d355
Ziyan Junaideen <ziyan@jdeen.com>
2023-11-25 20:37:37 +0530

    Thor CLI app

---
+++ lib/id_documents/cli.rb
@@ -0,0 +1,38 @@
+require "thor"
+require "artii"
+
+class IDDocuments::CLI < Thor
+  desc "parse ID_NUMBER", "Determins the validity of the ID number and extract metadata"
+  method_option :country, alias: "-c", type: :string, required: true
+  method_option :type, alias: "-t", type: :string, required: true
+  method_option :number, alias: "-n", type: :string, required: true
+  def parse
+    puts IDDocuments.parse(options[:country].to_sym, options[:type].to_sym, options[:number]).pp
+  end
+
+  desc "about", "About IDDocuments"
+  def about
+    artii = Artii::Base.new font: "standard"
+    message = """
+#{artii.asciify("IDDocuments")}
+ by Ziyan Junaideen
+
+ A variety of documents are used globally to verify a person's identity.
+ Businesses could use document numbers and embedded metadata (date of birth,
+ gender, etc) in supported documents as a first line of defense against fraud.
+
+ IDDocuments intends to provide a unified interface to validate ID document
+ numbers.
+    """
+    puts message
+  end
+
+  desc "version", "Prints version"
+  def version
+    puts "IDDocuments version #{IDDocuments::VERSION}"
+  end
+
+  def self.exit_on_failure?
+    true
+  end
+end
